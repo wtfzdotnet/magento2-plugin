3d00de3dc9430626925f162a89d293f29411d3fa
Michael Roterman <michael@pay.nl>
2020-12-07 12:17:55 +0100

    backup

--- registration.php
+++
@@ -1,9 +0,0 @@
-<?php
-/**
- * Copyright © 2020 PAY. All rights reserved.
- */
-\Magento\Framework\Component\ComponentRegistrar::register(
-    \Magento\Framework\Component\ComponentRegistrar::MODULE,
-    'Paynl_Payment',
-    __DIR__
-);
